303126dc2eb593afd703028228912696474cb38d
SHI ZHEN <shi.zhen@vincit.fi>
2020-11-07 13:36:11 +0200

    feat(8.4): Book author query

--- backend/index.js
+++ backend/index.js
@@ -100,16 +100,20 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
-
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((book) => book.author === args.author)
+      }
+      return books
+    },
     allAuthors: () => {
       return authors.map((author) => {
         const bookCount = books.reduce(((init, book) => book.author === author.name ? init + 1 : init), 0)
