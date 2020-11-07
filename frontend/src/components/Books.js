eeeb3d0fd78db331eb9cb8b91448a1494f65152c
SHI ZHEN <shi.zhen@vincit.fi>
2020-11-07 20:11:37 +0200

    feat(8.19): Genre filter

--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { gql, useQuery } from '@apollo/client';
 
 const ALL_PERSONS = gql`
@@ -10,11 +10,12 @@ query {
     }
     published
     id
+    genres
   }
 }
 `
 const Books = (props) => {
-
+  const [selectedGenre, setGenre] = useState()
   const result = useQuery(ALL_PERSONS)
 
   if (!props.show) {
@@ -26,6 +27,10 @@ const Books = (props) => {
     return <div>loading...</div>
   }
 
+  const genersList = result.data.allBooks
+    .reduce((init, { genres }) => init.concat(genres), [])
+  const uniqueList = genersList.filter((item, pos) => genersList.indexOf(item) == pos)
+  console.log("genersList", uniqueList)
   return (
     <div>
       <h2>books</h2>
@@ -41,15 +46,20 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {result.data.allBooks.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
+          {result.data.allBooks.
+            filter((a) => selectedGenre ? a.genres.includes(selectedGenre) : a)
+            .map(a =>
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )}
         </tbody>
       </table>
+      {
+        uniqueList.map((genre) => <button key={genre} onClick={() => setGenre(genre)}>{genre}</button>)
+      }
     </div>
   )
 }
